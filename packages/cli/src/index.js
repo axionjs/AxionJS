5fb74b00ed773c29a151b66a2fa57ab43e7d18eb
Minahil Ismail <minahilismail44@gmail.com>
2025-01-17 15:41:53 +0500

    feat(cli):add command

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -1,26 +1,24 @@
 #!/usr/bin/env node
 
 import { init } from "./commands/init.js";
-// import { add } from "../commands/add.js";
+import { add } from "./commands/add.js";
 import { Command } from "commander";
-import { readFileSync } from 'fs';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
+import { readFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const packageJsonPath = resolve(__dirname, '../package.json');
-const packageJson = JSON.parse(readFileSync(packageJsonPath, 'utf8'));
-// import packageJson from "../package.json";
-// import packageJson from "../package.json" assert { type: "json" };
+const packageJsonPath = resolve(__dirname, "../package.json");
+const packageJson = JSON.parse(readFileSync(packageJsonPath, "utf8"));
 
 process.on("SIGINT", () => process.exit(0));
 process.on("SIGTERM", () => process.exit(0));
 
 async function main() {
   const program = new Command()
-    .name("axionjs")
+    .name("testaxionjs")
     .description("Add components right in to your project")
     .version(
       packageJson.version || "1.0.0",
@@ -28,7 +26,7 @@ async function main() {
       "display the version number"
     );
 
-  program.addCommand(init);
+  program.addCommand(init).addCommand(add);
 
   program.parse();
 }
