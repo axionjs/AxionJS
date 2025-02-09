ef3e5a89e8c68dd93ffabe4020eb8aa6048622f2
Minahil Ismail <minahilismail44@gmail.com>
2025-02-09 13:39:19 +0500

    removed cli folder

--- packages/cli/src/utils/errors.js
+++
@@ -1,7 +0,0 @@
-export const MISSING_DIR_OR_EMPTY_PROJECT = "1";
-export const EXISTING_CONFIG = "2";
-export const MISSING_CONFIG = "3";
-export const FAILED_CONFIG_READ = "4";
-export const TAILWIND_NOT_CONFIGURED = "5";
-export const IMPORT_ALIAS_MISSING = "6";
-export const UNSUPPORTED_FRAMEWORK = "7";
