c9e8ff141b0b78112df718c861c7b4fd26a7b5f8
hanzala267 <mhanzala267@gmail.com>
2025-07-03 23:54:12 +0500

    reflector: footer

--- apps/docs/app/components/docs/site-footer.tsx
+++ apps/docs/app/components/docs/site-footer.tsx
@@ -11,11 +11,11 @@ export const siteConfig = {
 
 export function SiteFooter() {
   return (
-    <footer className="group-has-[.section-soft]/body:bg-surface/40 my-6 3xl:fixed:bg-transparent dark:bg-transparent">
+    <footer className="group-has-[.section-soft]/body:bg-surface/40 my-6 3xl:fixed:bg-transparent dark:bg-transparent text-primary">
       <div className="container-wrapper px-4 xl:px-6">
         <div className="flex h-(--footer-height) items-center justify-between">
           <div className="text-muted-foreground w-full text-center text-xs leading-loose sm:text-sm">
-            Built by AxionJS at . The source code is available on{" "}
+            Built by AxionJS at NTU. The source code is available on{" "}
             <a
               href={siteConfig.links.github}
               target="_blank"
